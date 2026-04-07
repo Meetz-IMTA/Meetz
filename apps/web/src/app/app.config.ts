8a64411da0e5d83922771ee7531d787efdcf3b96
Haithem <haithem.hadj-azzem@etu.univ-lyon1.fr>
2026-04-07 12:04:36 +0200

    chore: setup prisma, mariadb docker, primeng theme

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,11 +1,41 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { definePreset } from '@primeng/themes';
 import { routes } from './app.routes';
+
+const MeetzTheme = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#fff5f5',
+      100: '#ffe3e3',
+      200: '#ffc9c9',
+      300: '#ffa8a8',
+      400: '#ff8787',
+      500: '#ff6b6b',
+      600: '#fa5252',
+      700: '#f03e3e',
+      800: '#e03131',
+      900: '#c92a2a',
+      950: '#a61e1e'
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: MeetzTheme,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+        }
+      },
+      ripple: true
+    })
   ]
 };

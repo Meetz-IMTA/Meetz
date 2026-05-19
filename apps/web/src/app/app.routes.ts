e23b73b79ee62319652f17252ddc6d3a54479482
haithem <haithemly69@gmail.com>
2026-05-19 09:40:07 +0200

    refactor: remove register component

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-import { Register } from './pages/register/register';
 import { VerifyOtp } from './pages/verify-otp/verify-otp';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'register', component: Register },
   { path: 'verify-otp', component: VerifyOtp },
   { path: 'home', component: Home },
 ];

b04abf9225f20aee4f257829956ff40b8353c0ea
haithem <haithemly69@gmail.com>
2026-05-22 09:52:23 +0200

    feat: home

---
+++ apps/web/src/app/pages/home/components/categories-section/categories-section.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+interface Category {
+  label: string;
+  icon: string;
+  bgColor: string;
+  textColor: string;
+}
+
+@Component({
+  selector: 'app-categories-section',
+  imports: [],
+  templateUrl: './categories-section.html',
+})
+export class CategoriesSection {
+  readonly categories: Category[] = [
+    {
+      label: 'Sport',
+      icon: 'fitness_center',
+      bgColor: 'bg-blue-500/10',
+      textColor: 'text-blue-500',
+    },
+    {
+      label: 'Culture',
+      icon: 'theater_comedy',
+      bgColor: 'bg-purple-500/10',
+      textColor: 'text-purple-500',
+    },
+    {
+      label: 'Gastro',
+      icon: 'restaurant',
+      bgColor: 'bg-orange-500/10',
+      textColor: 'text-orange-500',
+    },
+    {
+      label: 'Jeux',
+      icon: 'sports_esports',
+      bgColor: 'bg-emerald-500/10',
+      textColor: 'text-emerald-500',
+    },
+    { label: 'Musique', icon: 'music_note', bgColor: 'bg-pink-500/10', textColor: 'text-pink-500' },
+    {
+      label: 'Savoir',
+      icon: 'lightbulb',
+      bgColor: 'bg-yellow-500/10',
+      textColor: 'text-yellow-500',
+    },
+  ];
+}
--- apps/web/src/app/pages/home/home.ts
+++ apps/web/src/app/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroSection } from './components/hero-section/hero-section';
+import { FeaturedEvents } from './components/featured-events/featured-events';
+import { CategoriesSection } from './components/categories-section/categories-section';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroSection, FeaturedEvents, CategoriesSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
